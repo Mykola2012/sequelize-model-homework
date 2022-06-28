c7b2e4c3c6058e0836fcdb645b823caeb978def8
Revika Mukola <revikanikolaj@gmail.com>
2022-06-28 10:33:01 +0200

    Add srver.js and app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const app = express();
+
+app.use(
+  express.json({
+    type: 'application/vnd.api+json',
+  })
+);
+
+app.use('/api', router);
+
+app.use((err, req, res, next) => {
+  if (res.headersSend) {
+    return;
+  }
+
+  const errorStatus = err?.status ?? 500;
+  res.status(errorStatus).send({
+    errors: [
+      {
+        status: errorStatus,
+        title: err.message ?? 'Internal Server Error',
+      },
+    ],
+  });
+});
+
+module.exports = app;
